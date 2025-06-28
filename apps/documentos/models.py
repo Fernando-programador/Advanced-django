6ae18eb31e1e47be0942bef5ab056150af614546
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2025-06-28 13:07:13 -0300

    create documentos e hora_extra
    relacionamento banco de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionario.models import Funcionario
 
 class Documentos(models.Model):
     descricao = models.CharField(max_length=255)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     
     
     def __str__(self):
--- apps/hora_extra/models.py
+++ apps/hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionario.models import Funcionario
 
 
 class RegistroExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
     
     
     def __str__(self):
